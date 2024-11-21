092a0cbcc95c0b95afdc3c03b612c75b24b77782
Андрей Зуев <andrej.zuev.2918@mail.ru>
2024-11-21 15:18:47 +0500

    Application Db context added

---
+++ Beatecho/DAL/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Beatecho.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Beatecho.DAL
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Song> Songs => Set<Song>();
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Album> Albums => Set<Album>();
+        public DbSet<Genre> Genres => Set<Genre>();
+        public DbSet<UserType> UserTypes => Set<UserType>();
+        public DbSet<SongGenre> SongGenres => Set<SongGenre>();
+
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(DbHelper.ConnString);
+        }
+    }
+}
